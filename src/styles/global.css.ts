0dbc07e14d8230947028b127e9db2eb6aa7990f1
HSEngin <103025622+HSEngin@users.noreply.github.com>
2025-04-21 23:18:22 -0700

    Create global.css.ts

---
+++ src/styles/global.css.ts
@@ -0,0 +1,5 @@
+// Import our global CSS
+import './global.css';
+
+// This file serves as a way for Astro to load global styles in dev and production
+// No content is needed here - just importing the CSS file is enough
